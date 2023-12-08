8d4fb75860a43a5ddf8bd28ff24d707aeb40d047
Wassim Assidi <w.assidi@arsela.co>
2023-12-08 12:03:55 +0100

    test 2

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const calculate = require('./calculate')
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Sum = ",calculate.sum(1,1));
 });
 
 app.listen(port, () => {
